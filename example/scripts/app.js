36b0e72a80f7bc38cba99ca146c766f73ba8615d
Hiroaki Yamamoto <admin@hysoftware.net>
2014-03-19 16:03:20 +0900

    Added modal show/hide trigger example.

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -1,6 +1,17 @@
 angular.module("exampleApp",["bswrapper"])
     .controller("exampleControl",["$scope",function($scope){
-        $scope.$on("buttonClicked",function(event,id){console.log(id)})
+        $scope.$on("buttonClicked",function(event,id){
+            switch(id){
+                case "attributeModal":
+                    console.log("Open: exampleModal as attribute")
+                break;
+                case "controlModal":
+                    console.log("Open: exampleModal as control")
+                    $scope.$broadcast("openModal","exampleModal")
+                break;
+            }
+        })
         $scope.$on("closeClicked",function(){console.log("Closed")})
         $scope.$on("hidden.bs.modal",function(){console.log("hidden")})
+        
     }])
